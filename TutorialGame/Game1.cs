bf55c528b7a399e6129c2caf90c261c16b68d68c
John Durham <jdurham2843@gmail.com>
2015-06-23 16:20:49 -0500

    Game in First Beta State

--- TutorialGame/Game1.cs
+++ TutorialGame/Game1.cs
@@ -15,8 +15,19 @@ namespace TutorialGame
         SpriteBatch spriteBatch;
         Vector2 playerScorePos;
         Vector2 enemyScorePos;
+        Vector2 winMessagePos;
+        string winMessage;
         int playerScore = 0;
         int enemyScore = 0;
+        enum GameState { playing, gameOver, splashscreen, menuScreen};
+        GameState gameState;
+
+        enum MenuState { Menu1, Menu2 };
+        MenuState menuState;
+
+        Texture2D splashScreen;
+        Texture2D menuImage1;
+        Texture2D menuImage2;
         
         
         
@@ -48,8 +59,10 @@ namespace TutorialGame
             GameObjectManager = new gameObjectManager();
             playerScorePos = new Vector2(200, 50);
             enemyScorePos = new Vector2(600, 50);
+            winMessagePos = new Vector2(400, 240);
             scoreFont = Content.Load<SpriteFont>("MyFont");
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            gameState = GameState.splashscreen;
             
 
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -60,6 +73,12 @@ namespace TutorialGame
             GameObjectManager.enemyPaddle.sprite = Content.Load<Texture2D>("paddle");
             GameObjectManager.Ball.sprite = Content.Load<Texture2D>("ball");
 
+
+            splashScreen = Content.Load<Texture2D>("PongSplashScreen");
+            menuImage1 = Content.Load<Texture2D>("PongMenu1");
+            menuImage2 = Content.Load<Texture2D>("PongMenu2");
+            menuState = MenuState.Menu1;
+            
             
             
         }
@@ -85,18 +104,67 @@ namespace TutorialGame
 
             //Game Logic
 
-            GameObjectManager.playerPaddle.movePaddle();
-            GameObjectManager.enemyPaddle.movePaddle();
-            GameObjectManager.Ball.moveBall();
-            GameObjectManager.Ball.ballCollision(GameObjectManager.playerPaddle.Rectangle, GameObjectManager.playerPaddle.player);
-            GameObjectManager.Ball.ballCollision(GameObjectManager.enemyPaddle.Rectangle, GameObjectManager.enemyPaddle.player);
-            GameObjectManager.Ball.ballCollision(GameObjectManager.screenTop, 0);
-            GameObjectManager.Ball.ballCollision(GameObjectManager.screenBottom, 0);
-            GameObjectManager.Ball.checkBounds(ref playerScore, ref enemyScore);
-
+            if (gameState == GameState.splashscreen)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Space) || Keyboard.GetState().IsKeyDown(Keys.Enter))
+                {
+                    gameState = GameState.menuScreen;
+                }
+            }
             
-            
+            else if (gameState == GameState.menuScreen)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Up) || Keyboard.GetState().IsKeyDown(Keys.Down))
+                {
+                    if (menuState == MenuState.Menu1)
+                        menuState = MenuState.Menu2;
+                    else if (menuState == MenuState.Menu2)
+                        menuState = MenuState.Menu1;
+                }
+                else if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+                {
+                    if (menuState == MenuState.Menu1)
+                    {
+                        gameState = GameState.playing;
+                    }
+                    else if (menuState == MenuState.Menu2)
+                    {
+                        Exit();
+                    }
+                }
 
+            }
+
+            else if (gameState == GameState.playing)
+            {
+                GameObjectManager.playerPaddle.movePaddle();
+                GameObjectManager.enemyPaddle.movePaddle();
+                GameObjectManager.Ball.moveBall();
+                GameObjectManager.Ball.ballCollision(GameObjectManager.playerPaddle.Rectangle, GameObjectManager.playerPaddle.player);
+                GameObjectManager.Ball.ballCollision(GameObjectManager.enemyPaddle.Rectangle, GameObjectManager.enemyPaddle.player);
+                GameObjectManager.Ball.ballCollision(GameObjectManager.screenTop, 0);
+                GameObjectManager.Ball.ballCollision(GameObjectManager.screenBottom, 0);
+                GameObjectManager.Ball.checkBounds(ref playerScore, ref enemyScore);
+
+                if (playerWin(playerScore))
+                {
+                    winMessage = "Player 1 Wins!!!";
+                    gameState = GameState.gameOver;
+                }
+                else if (enemyWin(enemyScore))
+                {
+                    winMessage = "Player 2 Wins!!!";
+                    gameState = GameState.gameOver;
+                }
+            }
+            else if (gameState == GameState.gameOver)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Space) || Keyboard.GetState().IsKeyDown(Keys.Enter))
+                {
+                    gameState = GameState.menuScreen;
+                }
+            }
+            
             base.Update(gameTime);
         }
 
@@ -109,14 +177,35 @@ namespace TutorialGame
             GraphicsDevice.Clear(Color.Black);
 
             spriteBatch.Begin();
-            spriteBatch.Draw(GameObjectManager.playerPaddle.sprite,
-                GameObjectManager.playerPaddle.Rectangle, Color.White);                        
-            spriteBatch.Draw(GameObjectManager.enemyPaddle.sprite,
-                GameObjectManager.enemyPaddle.Rectangle, Color.White);                        
-            spriteBatch.Draw(GameObjectManager.Ball.sprite,
-                GameObjectManager.Ball.Rectangle, Color.White);           
-            spriteBatch.DrawString(scoreFont, playerScore.ToString() ,playerScorePos, Color.White);
-            spriteBatch.DrawString(scoreFont, enemyScore.ToString(), enemyScorePos, Color.White);
+
+            
+
+            if (gameState == GameState.playing)
+            {
+                spriteBatch.Draw(GameObjectManager.playerPaddle.sprite,
+                    GameObjectManager.playerPaddle.Rectangle, Color.White);
+                spriteBatch.Draw(GameObjectManager.enemyPaddle.sprite,
+                    GameObjectManager.enemyPaddle.Rectangle, Color.White);
+                spriteBatch.Draw(GameObjectManager.Ball.sprite,
+                    GameObjectManager.Ball.Rectangle, Color.White);
+                spriteBatch.DrawString(scoreFont, playerScore.ToString(), playerScorePos, Color.White);
+                spriteBatch.DrawString(scoreFont, enemyScore.ToString(), enemyScorePos, Color.White);
+            }
+            else if (gameState == GameState.gameOver)
+            {
+                spriteBatch.DrawString(scoreFont, winMessage, winMessagePos , Color.White);
+            }
+            else if (gameState == GameState.splashscreen)
+            {
+                spriteBatch.Draw(splashScreen, new Rectangle(0, 0, 800, 480), Color.White);
+            }
+            else if (gameState == GameState.menuScreen)
+            {
+                if (menuState == MenuState.Menu1)
+                    spriteBatch.Draw(menuImage1, new Rectangle(0, 0, 800, 480), Color.White);
+                else
+                    spriteBatch.Draw(menuImage2, new Rectangle(0, 0, 800, 480), Color.White);
+            }
             spriteBatch.End();
 
             
@@ -125,6 +214,16 @@ namespace TutorialGame
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+
+        public bool playerWin(int p_playerScore)
+        {
+            return (playerScore == 10);
+        }
+
+        public bool enemyWin(int p_enemyScore)
+        {
+            return (enemyScore == 10);
         }
 
         
---
+++ TutorialGame/GameScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TutorialGame
+{
+    abstract class GameScreen
+    {
+        private Texture2D screenImage;
+
+        protected void Initialize()
+        {
+        }
+
+        protected void UnloadContent()
+        { }
+
+        protected void LoadContent(string imageName)
+        { 
+        }
+
+        protected void Update(GameTime gameTime)
+        { }
+
+        protected void Draw(SpriteBatch spriteBatch)
+        { }
+    }
+}
